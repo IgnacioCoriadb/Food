b9357bacad8f7c960eb962417276366f0f0ed1db
Ignacio <ignacioCoriadb@gmail.com>
2022-12-20 11:47:15 -0300

    Redux

---
+++ PI-Food-main/client/src/Redux/reducer/index.js
@@ -0,0 +1,19 @@
+import {GET_RECIPES} from "../actions/actionsType";
+
+const initialState ={
+    recipes: []
+}
+
+const rootReducer = (state = initialState, action) =>{
+    switch(action.type){
+        case GET_RECIPES: 
+            return {
+                ...state,
+                recipes: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default rootReducer;
