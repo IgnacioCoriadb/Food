3c3611487bdac7ccce0990ab90ee8efe6e286f3d
Ignacio <ignacioCoriadb@gmail.com>
2022-12-20 11:48:06 -0300

    navBar

---
+++ PI-Food-main/client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import style from "./NavBar.module.css";
+import logo from "../../img/logo.png";
+import {Link} from "react-router-dom";
+
+
+function NavBar(){
+    return (
+        <div className={style.backgroundColor}>
+            <div className={style.container}>
+                <div className={style.logo}>
+                    <Link to="/home"><img src={logo} alt="logo" /></Link>
+                </div>
+                <div>
+                    <ul className={style.links}>
+                        <Link to="/home" className={style.link}><li>Home</li></Link>
+                        <Link to="/" className={style.link}><li>Nueva Receta</li></Link>
+                        <Link to="/" className={style.link}><li>Dietas</li></Link>
+                        <Link to="/" className={style.link}><li>Proyecto</li></Link>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default NavBar;
+
